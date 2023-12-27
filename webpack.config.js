173a576d8c2f7f30a8fc77bda89de745bd010f59
unknown <danielsafari143@gmail.com>
2023-12-27 16:32:50 +0100

    ADD TAILWIND.CONFIG FILE

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  mode:"development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  mode: "development",
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
   },
 
   plugins: [
@@ -27,5 +42,4 @@ module.exports = {
       template: "index.html",
     }),
   ],
-
 };
