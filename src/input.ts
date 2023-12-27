07c9e9dd90a6cb67a9af079243e7c1721cc1bb84
unknown <danielsafari143@gmail.com>
2023-12-27 23:40:46 +0100

    ADD CHANGES

--- src/input.ts
+++ src/input.ts
@@ -1,8 +1,8 @@
-export default function Inputs(id: string): HTMLElement {
+export default function Inputs(id: string): void {
   const homepage: HTMLElement = document.getElementById(id);
   const input: HTMLInputElement = document.createElement("input");
   input.placeholder = "Typssomething about your quotes";
+  input.classList.add('border-2' , 'border-indigo-600')
+  homepage.appendChild(input);
   input.focus();
-  homepage.innerHTML += input;
-  return homepage.appendChild(input);
 }
