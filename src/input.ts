ec7bb4282ce526ac0ce9742003bfbfdab8f729ed
unknown <danielsafari143@gmail.com>
2023-12-28 16:11:03 +0100

    REFACTOR CODE

--- src/input.ts
+++ src/input.ts
@@ -1,7 +1,7 @@
 export default function Inputs(idContainer: string): void {
   let homepage: HTMLElement;
   const placeholder = `Share your thoughts, and I'll craft a quote to capture their essence.`;
-  const style = `ring-2 ring-slate-400 outline outline-slate-100 px-3 rounded-sm h-10 w-6/12`;
+  const style = `ring-2 ring-slate-500 outline outline-slate-100 px-3 rounded-sm h-10 w-6/12`;
 
   if (!document.getElementById(idContainer)) {
     throw new Error(
