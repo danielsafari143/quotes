f8a4b243ad1a3224e567efc67b06dccaf2eed4f4
unknown <danielsafari143@gmail.com>
2023-12-28 16:03:56 +0100

    ADD PLACEHOLDER AND REFACTOR

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import "./style.css";
 import Inputs from "./input";
 
-
 Inputs("homepage");
--- src/input.ts
+++ src/input.ts
@@ -1,15 +1,15 @@
-export default function Inputs(containerId: string): void {
-  const container = document.getElementById(containerId);
-  const inputElement = document.createElement("input");
+export default function Inputs(idContainer: string): void {
+  let homepage: HTMLElement;
+  const placeholder = `Share your thoughts, and I'll craft a quote to capture their essence.`;
+  const style = `ring-2 ring-slate-400 outline outline-slate-100 px-3 rounded-sm h-10 w-6/12`;
 
-  if (!container) {
-    throw new Error(`Container with ID "${containerId}" not found.`);
+  if (!document.getElementById(idContainer)) {
+    throw new Error(
+      `There is no parent component with this id: ${idContainer}`,
+    );
   }
 
-  inputElement.placeholder = "Type your thought and let the ai do the rest";
-  inputElement.classList.add("ring-1", "ring-black-500");
-
-  container.appendChild(inputElement);
+  homepage = document.getElementById(idContainer);
+  const input: string = `<input placeholder="${placeholder}" id="input" class="${style}"/>`;
+  homepage.innerHTML += input;
 }
-
-
