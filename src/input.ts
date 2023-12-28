9f6bd34d8aa40a86679fe870ae0553fb8b45e7cc
unknown <danielsafari143@gmail.com>
2023-12-28 15:23:57 +0100

    REFACTOR

--- src/input.ts
+++ src/input.ts
@@ -1,12 +1,12 @@
 export default function Inputs(containerId: string): void {
   const container = document.getElementById(containerId);
+  const inputElement = document.createElement("input");
 
   if (!container) {
     throw new Error(`Container with ID "${containerId}" not found.`);
   }
 
-  const inputElement = document.createElement("input");
-  inputElement.placeholder = "Something";
+  inputElement.placeholder = "Type your thought and let the ai do the rest";
   inputElement.classList.add("ring-1", "ring-black-500");
 
   container.appendChild(inputElement);
