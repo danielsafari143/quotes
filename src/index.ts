162ffb732b527aaeb47b9996d5c32329a98212eb
unknown <danielsafari143@gmail.com>
2023-12-31 00:59:37 +0100

    REFACTOR

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Inputs from "./input";
 import run from "./gemini";
 import posts from "./posts";
 
-const style = `ring-2 ring-slate-500 outline outline-slate-100 px-3 rounded-sm h-10 w-6/12`;
+const style = `ring-2 ring-slate-500 border border-gray-200 rounded-lg shadow outline outline-slate-100 px-3 h-10 w-6/12 focus:ring-white`;
 const placeholder = `Share your thoughts, and I'll craft a quote to capture their essence.`;
 const buttonStyle =
   "bg-blue-800 text-white p-2 ring-2 ring-blue-300 rounded-lg text-slate-500 hover:bg-blue-500 hover:ring-purple-200 hover:text-white  hover:text-bold";
